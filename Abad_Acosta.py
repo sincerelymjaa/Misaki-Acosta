1024f81dd9b05c1607e69f95ca43da858dadf0a3
Misaki Acosta <119102130+sincerelymjaa@users.noreply.github.com>
2023-08-17 15:01:46 +0800

    Add files via upload

--- Abad_Acosta.py
+++ Abad_Acosta.py
@@ -1,84 +1,65 @@
-"""
-Good day Prof! Here is our output. Since there are 8 classes,
-me and my partner divided it into two in accordance with what
-we did in Assignment 1. Thanks!
-"""
-
-# The succeeding lines are Mekyla Abad's contribution to this assignment.
-# This is the code for Class 1: Volleyball Ball
-class Volleyball_Ball:
-# Attributes of the Volleyball Ball class
-	def __init__(self, b, c, co, w, t):
-		self.brand = b
-		self.circumference = c
-		self.color = co
-		self.weight = w
-		self.type = t
-		
-# Methods of the Volleyball Ball class
-	def identify_brand(self):
-		self.brand = self.brand
-		
-	def measure(self):
-		self.measure = self.measure
-		
-	def distinguish_color(self):
-		self.color = self.color
-		
-	def weigh(self):
-		self.weigh = self.weigh
-		
-	def determine_type(self):
-		self.type = self.type
-
-#Created an object named 'vball'		
-vball = Volleyball_Ball("Mikasa", 65, "white", 270, "indoor")
-
-"""
-After running the code, it will show the brand, circumference,
-color, weight, and type of the Volleyball Ball. No change or 
-increase is needed since we are only describing the ball itself.
-"""
-
-print (vball.brand)
-print (vball.circumference)
-print (vball.color)
-print (vball.weight)
-print (vball.type)
-
-# This is the code for Class 2: Cellphone
+# The succeeding lines are Mekyla Abad's contribution to this Assignment
+#This is the code for the Cellphone Class (Parent Class)
 class Cellphone:
-# Attributes of the Cellphone class
-	def __init__(self, on, o, l, b, u):
-		self.on = on
-		self.off = o
-		self.locked = l
-		self.battery = b
-		self.unit = u
+# Attributes of the Cellphone Class
+    def __init__(self, on, o, l, b, u):
+	    self.on = on
+	    self.off = o
+	    self.locked = l
+	    self.battery = b
+	    self.unit = u
+
+# Methods for Cellphone Class
+    def turn_on(self):
+	    self.on = self.on
 	
-# Methods of the Cellphone class	
-	def turn_on(self):
-		self.on = self.on
-	
-	def turn_off(self):
-		self.off = self.off
+    def turn_off(self):
+	    self.off = self.off
 		
-	def typepassword_locked(self):
-		self.locked = self.locked
+    def typepassword_locked(self):
+	    self.locked = self.locked
 		
-	def charge_battery(self):
-		self.battery = self.battery
+    def charge_battery(self):
+	    self.battery = self.battery
 		
-	def identify_unit(self):
-		self.unit = self.unit
-	
-#Created an object named 'cp'	
-cp = Cellphone("on", "off", "love", "low battery", "Samsung A50s")
+    def identify_unit(self):
+	    self.unit = self.unit
+
+# This is the code for the Samsung Class (Child Class)
+class Samsung(Cellphone):
+# Attributes of the Samsung Class
+    def __init__(self, c, n, a, br, on, o, l, b, u):
+        super().__init__(on, o, l, b, u)
+        self.connection = c
+        self.notification = n
+        self.__application = a
+        self.brightness = br
+        
+# Methods for the Samsung Class
+    def fix_connection(self):
+        print ("Connection: ", self.connection)
+    def manage_notification(self):
+        print ("Notification: ", self.notification)
+    def adjust_brightness(self):
+        print ("Brightness: ", self.brightness)
+    
+# Getter and Setter methods for the private variable application
+    def set_application(self, a):
+        self.__application = a
+        
+    def get_application(self):
+        print(self.__application)
 
 """
-The attributes shows the class' status and what type it is
-while the methods show what to do to achieve the attributes.
-"""
+I set the attribute application into private because in reality, there are applications
+in our phones that we don't want anyone to access immediately, especially those that
+involves personal information and important stuff. As much as possible, we want privacy 
+and would not like others to know things that they shouldn't know about.
+"""  
+
+# Main Program
+#Created an object named 'cp' for the Parent Class, which is Cellphone	
+cp = Cellphone("on", "off", "love", "low battery", "Samsung A50s")
 
 print (cp.on)
 print (cp.off)
@@ -86,116 +67,52 @@ print (cp.locked)
 print (cp.battery)
 print (cp.unit)
 
-# This is the code for Class 3: Television
-class Television:
-# Attributes of the Television class
-	def __init__(self, v, c, i, p, m):
-		self.volume = v
-		self.channel = c
-		self.input = i
-		self.power = p
-		self.menu = m
-	
-# Methods of the Television class
-	def increase_volume(self):
-	 self.volume = self.volume + 7
- 
-	def change_channel(self):
-		self.channel = self.channel + 3
-		
-	def choose_input(self):
-		self.input = self.input
-	
-	def on_power(self):
-	 self.power = self.power
-	 
-	def brightnessfix_menu(self):
-		self.menu = self.menu -5
-	
-#Created an object named 'tv'	
-tv = Television(31, 2, "HDMI", "on", 25)
-
 """
-For this class, this is where we can do some modifications
-because the methods use verbs such as increase, change, and 
-fix. It is applicable since the example we have is a
-television that requires operation.
+After running the code above, it will show the on and off status of the phone, the password love
+to unlock the attribute 'locked,' notify that it is 'low battery' already, and that the phone's unit is 
+Samsung A50s.
 """
 
-print ("Before increase volume:", tv.volume)
-tv.increase_volume()
-print ("After increase volume:", tv.volume)
+# Created an object named 'kyla' for the Child Class, which is Samsung
+kyla = Samsung("Sky Fiber", "Detailed", "Insert private application", "600 nits", "on", "off", "kylavers", "Fully charged","Samsung Galaxy Z Flip 5")
 
-print ("Before change channel:", tv.channel)
-tv.change_channel()
-print ("After change channel:", tv.channel)
+print (kyla.connection)
+print (kyla.notification)
+print (kyla.brightness)
+print (kyla.on)
+print (kyla.off)
+print (kyla.locked)
+print (kyla.battery)
+print (kyla.unit)
 
-print (tv.input)
-print (tv.power)
-
-print ("Before brightnessfix_menu:", tv.menu)
-tv.brightnessfix_menu()
-print ("After brightnessfix_menu:", tv.menu)
-
-# This is the code for Class 4: Shirt
-class Shirt:
-# Attributes of the Shirt class
-	def __init__(self, s, c, b, d, p):
-		self.size = s
-		self.color = c
-		self.brand = b
-		self.design = d
-		self.price = p
-	
-# MEthods of the Shirt class
-	def determine_size(self):
-		self.size = self.size
-		
-	def recognize_color(self):
-	 self.color = self.color
-		
-	def identify_brand(self):
-		self.brand = self.brand
-		
-	def know_design(self):
-		self.design = self.design
-		
-	def check_price(self):
-		self.price = self.price +249
-
-#Created an object named 'sh'	
-sh = Shirt("small", "purple", "Adidas", "floral", 750)
+# Code to make the private attribute application publicly visible 
+kyla.get_application()
+kyla.set_application("GCash")
+kyla.get_application()
 
 """
-The first four attributes describe the look of the shirt.
-On the other hand, the last attribute refers to its price.
-There is an additional 249 pesos because upon checking in
-the physical store, the buyer discovers it is more expensive
-than the one being sold online.
+The object 'cp' for the parent class Cellphone is only an example of common 
+characteristics of a phone. It can be turn on and off, has a password, 
+display the battery status, and sample unit of a phone. On the other hand,
+the object 'kyla'for the child class Samsung is somehow more specific. Aside
+from the fact that it can also be turn on and off, type a password to unlock,
+show the battery level, and the phone's unit, it also reveals the cellphone's
+WiFi connection, notification setting, brightness level, and an application 
+that requires privacy.
 """
 
-print (sh.size)
-print (sh.color)
-print (sh.brand)
-print (sh.design)
-
-print ("Before check price:", sh.price)
-sh.check_price()
-print ("After check price:", sh.price)
-
 """
-Hello Prof!
-Doing this activity is really hard and overwhelming
-for me (since I'm only a beginner). It may seem very 
-very simple, but I did it on my own and I did my best 
-to accomplish it. Hope you appreciate my work.
--Mekyla Patrice G. Abad
+Indeed, inheritance allows code reusability and encapsulation allows us to
+restrict access from information we want to be protected. This activity 
+allowed us to practice the lessons and principles discussed from the previous
+weeks. It is hard, especially for a beginner like me, but if there's the will
+to learn, then anything can be possible.
 """
 
-# The succeeding lines are Misaki Acosta's contribution to this assignment.
-# This is the code for Class 5: K-Pop Album
+# The succeeding lines are Misaki Acosta's contribution to this Assignment
+# This is the code for the K-Pop Album Class (Parent Class)
 class KPopAlbum:
-# Attributes of K-Pop Album Class
+# Attributes of the K-Pop Album Class
     def __init__(self, v, p, pc, pb, c):
         self.version = v
         self.poster = p
@@ -219,7 +136,38 @@ class KPopAlbum:
     def play_cd(self):
         self.cd = self.cd 
 
-# Created Different K-Pop Album Variations
+# This is the code for the Pre-Order Benefits (POB) Class (Child Class)
+class POB(KPopAlbum):
+# Attributes of the POB Class
+    def __init__(self, e, b, f, s, v, p, pc, pb, c):
+        super().__init__(v, p, pc, pb, c)
+        self.exclusive_photocard = e
+        self.__videocall_benefit = b
+        self.folder = f
+        self.sticker = s
+        
+# Methods for the POB Class
+    def pull_random_exclusive_photocard(self):
+        print ("Exclusive Photocard: ", self.exclusive_photocard)
+    def get_random_a4_folder(self):
+        print ("A4 Folder: ", self.folder)
+    def obtain_random_sticker_set(self):
+        print ("Sticker: ", self.sticker)
+    
+# Getter and Setter methods for the private variable videocall_benefit
+    def set_videocall_benefit(self, b):
+        self.__videocall_benefit = b
+        
+    def get_videocall_benefit(self):
+        print(self.__videocall_benefit)
+
+"""
+The attribute videocall_benefit is private since, in a real-life context, 
+the information on who the winners are in the raffled video call benefit is not publicly disclosed. 
+Only the winners of the video call benefit know if they won the prize. 
+"""  
+# Main Program
+# Created Different Regular K-Pop Album Variations
 albumone = KPopAlbum("artbook","flat displaying a group photo", "Andy Park", "Page 3", "Track 11")
 albumtwo = KPopAlbum("vending machine", "flat displaying a photo of Donghyuck and Injun", "Taeraesa Kim", "Page 23", "Track 1")
 albumthree = KPopAlbum("limited", "flat displaying a photo of Haobin", "Sky Kim", "Page 14", "Track 5")
@@ -232,140 +180,43 @@ print (albumfour.photobook)
 print (albumfive.cd)
 
 """
-After running the code above, it will display the album version, poster image, photocard version 
-(which shows the name of the K-Pop Idol), photobook page, and track played on the album CD.
+After running the code above, it will display the album version, poster image, photocard version (which shows the name of the K-Pop Idol), photobook page, and track played on the album CD.
 I made five album variations to show the randomness of the inclusions that come with every K-Pop Album.
 I am only describing the inclusions; hence, there is no numerical change or increase in value.
 """
 
-# This is the code for Class 6: Digital Camera
-class DigiCam:
-# Attributes of Digital Camera Class
-    def __init__(self, l, f, vf, s, mc):
-        self.lens = l
-        self.flash = f
-        self.viewfinder = vf 
-        self.shutter = s
-        self.memorycard = mc 
+# Created a Special/Limited Edition K-Pop Album which includes the POB
+specialalbum = POB("Charisma Hao (Cat Ver.)", "Insert Winner's Name Here", "Dear Dream A4 Clear Folder", "S-Class Sticker Set", "mixtape", "flat displaying a group yearbook layout", "Shen Quan Rui","Page 27", "Track 3")
 
-# Methods for Digital Camera Class
-    def change_lens(self):
-        self.lens = self.lens + 10
-    
-    def adjust_flash(self):
-        self.flash = self.flash + 0.5
-        
-    def peek_through_viewfinder(self):
-        self.viewfinder = self.viewfinder
-    
-    def press_shutter(self):
-        self.shutter = self.shutter
-        
-    def insert_memorycard(self):
-        self.memorycard = self.memorycard + 256
+print (specialalbum.exclusive_photocard)
+print (specialalbum.folder)
+print (specialalbum.sticker)
+print (specialalbum.version)
+print (specialalbum.poster)
+print (specialalbum.photocard)
+print (specialalbum.photobook)
+print (specialalbum.cd)
 
-# Created a Camera Object
-camera = DigiCam(360, 9, "You saw the white sandy beach through the viewfinder", "The shutter was pressed", 0)
-print ("Before change lens:", camera.lens)
-camera.change_lens()
-print ("After change lens:", camera.lens)
-print ("Before adjust flash:", camera.flash)
-camera.adjust_flash()
-print ("After adjust flash:", camera.flash)
-print (camera.viewfinder)
-print (camera.shutter)
-print ("Before insert memory card:", camera.memorycard)
-camera.insert_memorycard()
-print ("After insert memory card:", camera.memorycard)
+# Code to make the private attribute videocall_benefit publicly visible 
+specialalbum.get_videocall_benefit()
+specialalbum.set_videocall_benefit("Akiara Acosta-Kim")
+specialalbum.get_videocall_benefit()
 
 """
-After running the code above, it will display the change in the numerical value of the lens millimeter 
-(which affects the focal length of the camera), flash value, and memory card storage value. 
-It also describes what a person can see through their viewfinder and if the camera's shutter is pressed.
-I tried simulating the way a person uses a camera through code.
+Regular Albums and Special Albums share attributes such as the album version, poster image, photocard version (which shows the name of the K-Pop Idol), photobook page, and track played on the album CD.
+However, unlike the other Regular K-Pop Albums above, the Special Album has Pre-Order Benefits (POB). These Pre-Order benefits are rare; only those fans who ordered the album early or its limited version can have them. 
+The most common POBs are exclusive photocards and the video call benefit. The exclusive photocards contain a rare selfie taken by the K-Pop Idol. As for the video call benefit, fans who win this can interact with their K-Pop idol through a video call. 
+As for the folder and sticker, it is usually a "limited edition" benefit for the K-Pop Album's Japan release. 
+Fans can only get these benefits if they purchase the album from a specific shop (like Makestar, Withmuu, etc.) or a CD store in Japan. 
+However, the POB from one store differs from that from another store.
+No POBs are the same from various stores.
 """
 
-# This is the code for Class 7: Guitar
-class Guitar:
-# Attributes of Guitar Class
-    def __init__(self, st, fb, t, b, n):
-        self.strings = st
-        self.fretboard = fb
-        self.type = t 
-        self.body = b
-        self.neck = n
-
-# Methods for Guitar Class
-    def tune_strings(self):
-        self.strings = self.strings + 1
-    
-    def strum_chord(self):
-        self.fretboard = self.fretboard
-    
-    def determine_type(self):
-        self.type = self.type
-        
-    def knock_on_body(self):
-        self.body = self.body
-        
-    def hold_neck(self):
-        self.neck = self.neck
-
-# Created a Guitar Object
-guitarnitaerae = Guitar(0, "You strummed the G Chord", "Yamada Acoustic Guitar", "You knocked on your guitar's body", "You are holding your guitar by its neck")
-print ("Before tune strings:", guitarnitaerae.strings)
-guitarnitaerae.tune_strings()
-print ("After tune strings:", guitarnitaerae.strings)
-print(guitarnitaerae.fretboard)
-print(guitarnitaerae.type)
-print(guitarnitaerae.body)
-print(guitarnitaerae.neck)
-
 """
-After running the code above, it will display the increase in the numerical value of the tuned chord. 
-This change simulates the increase in the chord's pitch once you twist the tuning peg for that chord.
-The code also describes the different actions a person does with their guitar. It also notes the guitar type. 
-I had to modify one of the methods so that all of the attributes have a corresponding method.
-"""
-
-# This is the code for Class 8: Computer Mouse
-class CompMouse:
-# Attributes of Computer Mouse Class
-    def __init__(self, c, w, wh, bt, tp):
-        self.color = c
-        self.weight = w
-        self.wheel = wh 
-        self.button = bt
-        self.type = tp
-
-# Methods for Computer Mouse Class
-    def see_color(self):
-        self.color = self.color 
-    
-    def discover_weight(self):
-        self.weight = self.weight + 4.23
-
-    def scroll_up(self):
-        self.wheel = self.wheel
-    
-    def click_left_button(self):
-        self.button = self.button
-        
-    def determine_type(self):
-        self.type = self.type
-
-# Created a Mouse Object
-desktopmouse = CompMouse("black", 0, "You scrolled up", "You left-clicked", "wireless mouse")
-print(desktopmouse.color)
-print ("Before discover weight:", desktopmouse.weight)
-desktopmouse.discover_weight()
-print ("After discover weight:", desktopmouse.weight)
-print(desktopmouse.wheel)
-print(desktopmouse.button)
-print(desktopmouse.type)
-
-""" 
-After running the code above, it will display the color, weight, and type of the Computer Mouse. 
-The code also describes the actions taken by the person if they use the mouse wheel to scroll and click its button.
-I had to modify one of the methods so that all of the attributes have a corresponding method.
+For this final assignment, I combined my interest in K-Pop with the concepts of OOP we learned this trimester, which made the coding process fun. 
+At first, I coded my setters and getters wrong since I realized I missed an underscore in one line of code. 
+Upon coming to this realization, I added the missing underscore. 
+Adding this helped display my private attribute. 
+Overall, I had fun learning more about coding and Python this trimester! 
+I also hope that through my code, you can learn more about the different variations of K-Pop albums. 
 """
